b209850457d44f592718f6f6e082c6cba9a1028b
Fede-rei <federei05@gmail.com>
2024-05-06 18:31:55 +0200

    rimosso (commentato) carosello non funzionante

--- Pages/index.php
+++ Pages/index.php
@@ -40,16 +40,15 @@ $rootPath = '../'
 <body>
 <?php include 'header.php'; ?>
 
-<div>
+<!--<div>
     <div id="myCarousel" class="carousel-slide" data-ride="carousel">
-        <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
             <li data-target="#myCarousel" data-slide-to="2"></li>
         </ol>
 
-        <!-- Wrapper for slides -->
+
         <div class="carousel-inner">
             <div class="item-active">
                 <img src="Images/0.png">
@@ -64,7 +63,6 @@ $rootPath = '../'
             </div>
         </div>
 
-        <!-- Left and right controls -->
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left"></span>
             <span class="sr-only">Previous</span>
@@ -74,7 +72,7 @@ $rootPath = '../'
             <span class="sr-only">Next</span>
         </a>
     </div>
-</div>
+</div> -->
 <?php include '../include/footer.php'; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -46,16 +46,15 @@ $rootPath = './';
     <body>
         <?php include 'include/header.php'; ?>
 
-        <div>
+        <!--<div>
             <div id="myCarousel" class="carousel-slide" data-ride="carousel">
-                <!-- Indicators -->
                 <ol class="carousel-indicators">
                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                     <li data-target="#myCarousel" data-slide-to="1"></li>
                     <li data-target="#myCarousel" data-slide-to="2"></li>
                 </ol>
 
-                <!-- Wrapper for slides -->
+
                 <div class="carousel-inner">
                     <div class="item-active">
                         <img src="Images/0.png" class="img-carousel">
@@ -70,7 +69,7 @@ $rootPath = './';
                     </div>
                 </div>
 
-                <!-- Left and right controls -->
+
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left"></span>
                     <span class="sr-only">Previous</span>
@@ -80,7 +79,7 @@ $rootPath = './';
                     <span class="sr-only">Next</span>
                 </a>
             </div>
-        </div>
+        </div>-->
 <?php include 'include/footer.php'; ?>
 </body>
 </html>
