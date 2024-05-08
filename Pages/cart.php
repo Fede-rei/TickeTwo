1036b1bf928ae7218080523c1001541ff6e59ec7
ChriFacc <christian.chinaglia@isisfacchnetti.edu.it>
2024-05-08 13:46:04 +0200

    aggiunti commenti

--- Pages/cart.php
+++ Pages/cart.php
@@ -1,20 +1,24 @@
 <?php
+// Avvia la sessione
 session_start();
 
+// Cancella l'ID dell'evento dalla sessione
 unset($_SESSION['eventId']);
 
+// Se l'utente non ha effettuato l'accesso, reindirizza alla pagina di login
 if(!isset($_SESSION['user'])) {
     header('Location: login.php');
 }
 
-
-
+// Imposta il percorso radice del sito
 $rootPath = '../';
 
+// Includi il file di configurazione del database e le operazioni del carrello
 include '../include/db_inc.php';
 include '../include/operazioni_cart.php';
 
-if(!isset($_SESSION['pic'])&&isset($_SESSION['user'])) {
+// Se l'utente ha effettuato l'accesso ma non ha ancora un'immagine del profilo nella sessione, recupera l'immagine dal database
+if(!isset($_SESSION['pic']) && isset($_SESSION['user'])) {
     if (isset($db)) {
         $pfpq = $db->query('select pfp from utente where id_utente = ' . $_SESSION['user']);
         $pfp = $pfpq->fetch()['pfp'];
@@ -23,28 +27,34 @@ if(!isset($_SESSION['pic'])&&isset($_SESSION['user'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>TikeTwo - Carrello</title>
-        <link rel="stylesheet" href="../Styles/header-footer.css">
-        <link rel="stylesheet" href="../Styles/cart.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-        <link href="../Images/ticketwoW.png" rel="icon" type="image/png">
-        <script src="../Scripts/counter.js" defer></script>
-    </head>
+<head>
+    <title>TikeTwo - Carrello</title>
+    <!-- Includi i fogli di stile -->
+    <link rel="stylesheet" href="../Styles/header-footer.css">
+    <link rel="stylesheet" href="../Styles/cart.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="../Images/ticketwoW.png" rel="icon" type="image/png">
+    <!-- Includi lo script per il contatore dei biglietti -->
+    <script src="../Scripts/counter.js" defer></script>
+</head>
 
-    <body>
-        <?php include '../include/header.php'; ?>
+<body>
+<?php include '../include/header.php'; ?>
 
-        <div class="carrello">
-            <h1>Carrello</h1>
-            <hr>
-            <div class="cart">
-                <?php displayCart() ?>
-                <?php total() ?>
-            </div>
-        </div>
+<div class="carrello">
+    <!-- Titolo della pagina -->
+    <h1>Carrello</h1>
+    <!-- Linea divisoria -->
+    <hr>
+    <div class="cart">
+        <!-- Visualizza il contenuto del carrello -->
+        <?php displayCart() ?>
+        <!-- Visualizza il totale del carrello -->
+        <?php total() ?>
+    </div>
+</div>
 
-        <?php include '../include/footer.php'; ?>
-    </body>
+<?php include '../include/footer.php'; ?>
+</body>
 </html>
--- include/header.php
+++ include/header.php
@@ -1,17 +1,25 @@
 <header>
+    <!-- Link all'homepage del sito -->
     <a href="<?= $rootPath ?>index.php" id="ticketImg">
+        <!-- Immagine del logo del sito -->
         <img src="<?= $rootPath ?>Images/ticketwo.png" class="tI">
     </a>
+    <!-- Barra di ricerca -->
     <div class="searchBar">
+        <!-- Input di ricerca -->
         <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value=""/>
+        <!-- Pulsante di invio della ricerca -->
         <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
+            <!-- Icona di ricerca -->
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="#666666"
                       d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/>
             </svg>
         </button>
     </div>
+    <!-- Menu di navigazione -->
     <div id="menu">
+        <!-- Link al carrello o alla pagina di login a seconda dello stato dell'utente -->
         <a href="<?php
 
         if (isset($_SESSION['user'])) {
@@ -21,17 +29,23 @@
         }
 
         ?>">
+            <!-- Icona del carrello -->
             <span class="material-symbols-outlined">
                 shopping_cart
             </span>
         </a>
+        <!-- Icona dell'utente -->
         <div class='uIcon'>
             <?php if (isset($_SESSION['user'])) { ?>
+                <!-- Link al profilo utente -->
                 <a href="<?= $rootPath ?>Pages/account.php">
+                    <!-- Immagine del profilo utente -->
                     <img class='user' value="<?= $rootPath ?>" src='<?= $rootPath ?>pfp/<?= $_SESSION['pic'] ?>'>
                 </a>
             <?php } else { ?>
+                <!-- Link alla pagina di login -->
                 <a href="<?= $rootPath ?>Pages/login.php">
+                    <!-- Immagine del profilo utente -->
                     <img class='user' value="<?= $rootPath ?>" src='<?= $rootPath ?>pfp/<?= $_SESSION['pic'] ?>'>
                 </a>
             <?php } ?>
