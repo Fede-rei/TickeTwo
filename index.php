41731d17d23ea006398f8c8b44c7f52495773372
ChriFacc <christian.chinaglia@isisfacchnetti.edu.it>
2024-05-08 19:50:51 +0200

    con swiper forse

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['pic']) && isset($_SESSION['user'])) {
         $_SESSION['pic'] = $pfpq->fetch()['pfp'];
     }
 }
-else
+elseif(!isset($_SESSION['user']))
     $_SESSION['pic'] = "0.png";
 
 // Imposta il percorso radice del sito
@@ -29,15 +29,8 @@ $rootPath = './';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Collegamenti ai fogli di stile -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link href="Images/ticketwoW.png" rel="icon" type="image/png">
-    <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
-    <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
-    <!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Collegamenti agli script JavaScript -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -47,12 +40,10 @@ $rootPath = './';
     <link rel="stylesheet" href="Styles/header-footer.css">
     <link rel="stylesheet" href="Styles/index.css">
     <script src="Scripts/search.js" defer ></script>
+
     <link rel="stylesheet" href="Styles/swiperStyle.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.8.4/swiper-bundle.min.css">
-
-
-
 </head>
 <body>
 <?php include 'include/header.php'; ?>
@@ -69,7 +60,7 @@ $rootPath = './';
         <!-- Immagini del carosello -->
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" id="imgHeight" src="Images/blu.jpg" alt="First slide">
+                <img class="d-block w-100" id="imgHeight" src="Images/ticketwo.png" alt="First slide">
                 <div class="carousel-caption d-none d-md-block">
                     <h3 class="eventText">BO</h3>
                 </div>
@@ -81,20 +72,22 @@ $rootPath = './';
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" id="imgHeight" src="Images/ticketwo.png" alt="Third slide">
+                <img class="d-block w-100" id="imgHeight" src="Images/blu.jpg" alt="Third slide">
                 <div class="carousel-caption d-none d-md-block">
                     <h3 class="eventText">BO</h3>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
+        <!-- Controlli del carosello -->
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
         <p class="text-center text-3 mt-3">Eventi Sportivi: </p>
         <?php include 'include/swiper.php'; ?>
@@ -121,6 +114,7 @@ $rootPath = './';
         </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.8.4/swiper-bundle.min.js"></script>
         <script src="script.js"></script>
+    </div>
 </body>
 
 <?php include 'include/footer.php'; ?>
