2d66c465c23a30cc2b18a04be8d35f72f9fcf4f8
Fede-rei <federei05@gmail.com>
2024-05-08 19:07:41 +0200

    dani caroselli

--- index.php
+++ index.php
@@ -9,29 +9,32 @@ $rootPath = './';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>TickeTwo - Home</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link href="Images/ticketwoW.png" rel="icon" type="image/png">
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
-<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
-<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
+    <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
+    <!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="Styles/header-footer.css">
     <link rel="stylesheet" href="Styles/index.css">
     <link rel="stylesheet" href="Styles/swiperStyle.css">
-    <script src="Scripts/swiperScript.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.8.4/swiper-bundle.min.css">
+
+
+
 </head>
+
+<body>
+
     <?php include 'include/header.php'; ?>
 
     <div>
@@ -43,13 +46,13 @@ $rootPath = './';
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" id="imgHeight" src="Images/ticketwo.png" alt="First slide">
+                    <img class="d-block w-100" id="imgHeight" src="Images/carosello1.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" id="imgHeight" src="Images/0.png" alt="Second slide">
+                    <img class="d-block w-100" id="imgHeight" src="Images/carosello2.jpeg" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" id="imgHeight" src="Images/blu.jpg" alt="Third slide">
+                    <img class="d-block w-100" id="imgHeight" src="Images/carosello3.jpg" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -62,37 +65,31 @@ $rootPath = './';
             </a>
         </div>
 
-        <!-- swiper eventi -->
+        <p class="text-center text-3 mt-3">Eventi Sportivi: </p>
+        <?php include 'include/swiper.php'; ?>
 
-        <div class="swiper-container" id="products">
+        <p class="text-center text-3 mt-3">Concerti: </p>
+        <?php include 'include/swiper.php'; ?>
 
-            <div class="swiper-wrapper">
-
-                <div class="swiper-slide">
-                    <ul>
-                        <li class="element"><img src="Images/0.png" class="product" ></li>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                    </ul>
-                </div>
-                <div class="swiper-slide">
-                    <ul>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                        <li class="element"><img src="Images/0.png" class="product"></li>
-                    </ul>
-                </div>
-            </div>
+        <p class="text-center text-3 mt-3">Fiere: </p>
+        <?php include 'include/swiper.php'; ?>
 
 
-            <div class="swiper-button-prev" ></div>
-            <div class="swiper-button-next" ></div>
-        </div>
-    </div>
 
+        <?php include 'include/footer.php'; ?>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-    <?php include 'include/footer.php'; ?>
-    </body>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.8.4/swiper-bundle.min.js"></script>
+        <script src="script.js"></script>
+</body>
+
 </html>
