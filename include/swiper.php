76d19b8ba35428f686fcb22fec3fd8dcc8fe9fe2
ChriFacc <christian.chinaglia@isisfacchnetti.edu.it>
2024-05-08 20:05:34 +0200

    in teoria funziona tutto

---
+++ include/swiper.php
@@ -0,0 +1,21 @@
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">Card 1</div>
+        <div class="swiper-slide">Card 2</div>
+        <div class="swiper-slide">Card 3</div>
+        <div class="swiper-slide">Card 4</div>
+        <div class="swiper-slide">Card 5</div>
+        <div class="swiper-slide">Card 6</div>
+        <div class="swiper-slide">Card 7</div>
+        <div class="swiper-slide">Card 8</div>
+        <div class="swiper-slide">Card 9</div>
+        <div class="swiper-slide">Card 10</div>
+        <div class="swiper-slide">Card 11</div>
+        <div class="swiper-slide">Card 12</div>
+        <div class="swiper-slide">Card 13</div>
+        <div class="swiper-slide">Card 14</div>
+        <div class="swiper-slide">Card 15</div>
+    </div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+</div>
