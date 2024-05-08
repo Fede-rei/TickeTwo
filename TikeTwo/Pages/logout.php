073b478371f037bf7fed0ca168c04e6d9905be8e
ChriFacc <christian.chinaglia@isisfacchnetti.edu.it>
2024-05-08 19:46:11 +0200

    sito funzionante

--- TikeTwo/Pages/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time()-1, '/');
-}
-session_destroy();
-header("Location:../index.php");
