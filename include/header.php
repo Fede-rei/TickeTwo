e60bdc412d8fca0abbd03327944090d199649ba0
Pelle <luca05pelle@gmail.com>
2024-05-08 08:16:31 +0200

    header href alla home

--- include/header.php
+++ include/header.php
@@ -1,22 +1,24 @@
-
-
 <header>
-    <img src="<?= $rootPath ?>Images/ticketwo.png" id="ticketImg">
+    <a href="<?= $rootPath ?>/index.php">
+        <img src="<?= $rootPath ?>Images/ticketwo.png" id="ticketImg">
+    </a>
     <div class="searchBar">
-        <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value="" />
+        <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value=""/>
         <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
-            <svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="#666666"
+                      d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/>
             </svg>
         </button>
     </div>
     <div id="menu">
         <a href="<?php
 
-            if(isset($_SESSION['user'])) {
-                echo $rootPath . "Pages/cart.php";
-            } else {
-                echo $rootPath . "Pages/login.php";
-            }
+        if (isset($_SESSION['user'])) {
+            echo $rootPath . "Pages/cart.php";
+        } else {
+            echo $rootPath . "Pages/login.php";
+        }
 
         ?>">
             <span class="material-symbols-outlined">
@@ -25,13 +27,13 @@
         </a>
         <div class='uIcon'>
             <?php if (isset($_SESSION['user'])) { ?>
-             <a href="<?= $rootPath ?>Pages/account.php">
-                <img class='user' value="<?= $rootPath ?>" src='<?= $rootPath ?>Images/0.png'>
-             </a>
+                <a href="<?= $rootPath ?>Pages/account.php">
+                    <img class='user' value="<?= $rootPath ?>" src='<?= $rootPath ?>Images/0.png'>
+                </a>
             <?php } else { ?>
-            <a href="<?= $rootPath ?>Pages/login.php">
-                <img class='user' value="<?= $rootPath ?>" src='<?= $rootPath ?>Images/0.png'>
-            </a>
+                <a href="<?= $rootPath ?>Pages/login.php">
+                    <img class='user' value="<?= $rootPath ?>" src='<?= $rootPath ?>Images/0.png'>
+                </a>
             <?php } ?>
         </div>
     </div>
