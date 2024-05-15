09e12c9a7dbfebec26a13bcd9c0bd0db0ed93c20
ChriFacc <christian.chinaglia@isisfacchnetti.edu.it>
2024-05-15 14:17:34 +0200

    trigger fatti da fede

--- db/ticketwo_db.sql
+++ db/ticketwo_db.sql
@@ -4,7 +4,7 @@ USE ticketwo;
 CREATE TABLE IF NOT EXISTS Utente(
     id_utente INT AUTO_INCREMENT,
     username VARCHAR(50),
-    password varchar(100),
+    password varchar(200),
     tipo BOOLEAN,
     mail VARCHAR(50),
     pfp VARCHAR(50),
@@ -39,6 +39,27 @@ CREATE TABLE IF NOT EXISTS Acquisti(
     FOREIGN KEY(id_cliente) REFERENCES Utente(id_utente),
     FOREIGN KEY(id_ticket) REFERENCES Biglietto(id_Biglietto)
 );
+
+CREATE TRIGGER controllo_quantità_biglietti
+    BEFORE INSERT ON Acquisti
+    FOR EACH ROW
+BEGIN
+    DECLARE biglietti_disponibili INT;
+    SELECT posti INTO biglietti_disponibili FROM Evento WHERE id_evento = NEW.id_ticket;
+    IF NEW.q > biglietti_disponibili THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Non ci sono abbastanza biglietti disponibili';
+    END IF;
+END;
+
+CREATE TRIGGER aggiornamento_posti_disponibili
+    AFTER INSERT ON Acquisti
+    FOR EACH ROW
+BEGIN
+    UPDATE Evento SET posti = posti - NEW.q WHERE id_evento = NEW.id_ticket;
+END;
+
+
+
 
 CREATE TABLE IF NOT EXISTS Carrello(
     id_carrello INT AUTO_INCREMENT,
