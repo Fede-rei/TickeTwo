56e1756501d89a8612ed298fe5890298fcb6f693
ChriFacc <christian.chinaglia@isisfacchnetti.edu.it>
2024-05-19 23:34:25 +0200

    css carosello

--- include/carosello.php
+++ include/carosello.php
@@ -5,11 +5,22 @@ if (isset($db)) {
     $events = $db->query('SELECT * FROM evento');
     $eventsArray = $events->fetchAll();
 }
+
+// Crea un array con i numeri da 1 a n
+$carEventN = range(0, count($eventsArray) - 1);
+
+// Mescola l'array
+shuffle($carEventN);
+
+// Seleziona i primi $count numeri
+$carEventN = array_slice($carEventN, 0, 3);
+
+$carEvents = array($eventsArray[$carEventN[0]], $eventsArray[$carEventN[1]], $eventsArray[$carEventN[2]]);
 ?>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <!-- Immagini del carosello -->
     <div class="carousel-inner">
-        <?php foreach ($eventsArray as $key => $item): ?>
+        <?php foreach ($carEvents as $key => $item): ?>
             <div class="carousel-item <?php echo $key === 0 ? 'active' : ''; ?>">
                 <a href="Pages/eventPage.php?eventId=<?= $item['id_evento'] ?>">
                 <img class="d-block w-100" id="imgHeight" src="Images/<?= $item['image']?>" alt="<?= $item['nome'] ?>">
